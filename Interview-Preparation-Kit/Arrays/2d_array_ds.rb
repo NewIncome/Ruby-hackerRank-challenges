981fa516d21d1e3a4b2736fe6a8bd3994742418f
Alfredo C <alfredo_cardenas@live.com>
2021-05-17 17:00:16 -0500

    Fix code, got a solution. Passed 7/9 TC. Wrong answer on 2

--- Interview-Preparation-Kit/Arrays/2d_array_ds.rb
+++ Interview-Preparation-Kit/Arrays/2d_array_ds.rb
@@ -3,12 +3,14 @@
 def hourglassSum(arr)
   # Write your code here
   prevVal = 0
-  while j < arr.length
-    newVal = arr[i][j] + arr[i+1][j] +  + arr[i+2][j]
-                        + arr[i+1][j+1]
-              arr[i][j+2] + arr[i+1][j+2] +  + arr[i+2][j+2]
+  i = 0
+  j = 0
+  while j < 4
+    newVal = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
     prevVal = newVal if newVal > prevVal
-    if i === arr.length-1
+    puts "i:#{i}, j#{j}, prev:#{prevVal}, new:#{newVal}"
+    i += 1
+    if i === 4
       i = 0
       j += 1
     end
@@ -22,3 +24,7 @@ puts hourglassSum([[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [
 puts hourglassSum([[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 9, 2, -4, -4, 0], [0, 0, 0, -2, 0, 0], [0, 0, -1, -2, -4, 0]])
 # T.C.2: 28
 puts hourglassSum([[-9, -9, -9, 1, 1, 1], [0, -9, 0, 4, 3, 2], [-9, -9, -9, 1, 2, 3], [0, 0, 8, 6, 6, 0], [0, 0, 0, -2, 0, 0], [0, 0, 1, 2, 4, 0]])
+
+23 33 43
+   34
+25 
