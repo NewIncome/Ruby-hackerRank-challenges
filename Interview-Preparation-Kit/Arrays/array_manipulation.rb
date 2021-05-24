1675abebee376d2253f0640aeae7b6268f7d05d7
Alfredo C <alfredo_cardenas@live.com>
2021-05-24 17:44:54 -0500

    Add code initialization

---
+++ Interview-Preparation-Kit/Arrays/array_manipulation.rb
@@ -0,0 +1,12 @@
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+
+def arrayManipulation(n, queries)
+  # Write your code here
+
+end
+
+# T.C.0:
+p arrayManipulation()
