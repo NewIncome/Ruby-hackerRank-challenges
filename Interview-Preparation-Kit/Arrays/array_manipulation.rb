66db72ff30c7f53881b64c4cc39e71e3063a991b
Alfredo C <alfredo_cardenas@live.com>
2021-05-26 17:45:14 -0500

    Add additional test case for edge case testing

--- Interview-Preparation-Kit/Arrays/array_manipulation.rb
+++ Interview-Preparation-Kit/Arrays/array_manipulation.rb
@@ -17,3 +17,6 @@ p arrayManipulation(5, [ [ 1, 2, 100 ], [ 2, 5, 100 ], [ 3, 4, 100 ] ])
 p arrayManipulation(10, [ [ 1, 5, 3 ], [ 4, 8, 7 ], [ 6, 9, 1 ] ])
 # T.C.2: 31
 p arrayManipulation(10, [ [ 2, 6, 8 ], [ 3, 5, 7 ], [ 1, 8, 1 ], [ 5, 9, 15 ] ])
+
+# T.C.01: 882
+p arrayManipulation(4, [[2,3,603],[1,1,286],[4,4,882]])
