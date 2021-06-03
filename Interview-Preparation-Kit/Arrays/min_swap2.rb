4589bb60ae36cfd58ca8daea49c537465470c8ac
Alfredo C <alfredo_cardenas@live.com>
2021-06-02 21:18:17 -0500

    Add challenge spec and the first 3 test cases

--- Interview-Preparation-Kit/Arrays/min_swap2.rb
+++ Interview-Preparation-Kit/Arrays/min_swap2.rb
@@ -1,6 +1,10 @@
 def minimumSwaps(arr)
-
+  # Find and return the minimum number of swaps required to sort the array in ascending order
 end
 
 # T.C.0:
-p minimumSwaps()
+p minimumSwaps([4,3,1,2])
+# T.C.1:
+p minimumSwaps([2,3,4,1,5])
+# T.C.2:
+p minimumSwaps([1,3,5,2,4,6,7])
