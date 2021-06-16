f2d6e40a6c3a68a309275a2fa82b989ea757ddc5
Alfredo C <alfredo_cardenas@live.com>
2021-06-16 14:38:46 -0500

    Add default code for the coding challenge

---
+++ Microverse-Coding-Challenges/drawing_book.rb
@@ -0,0 +1,10 @@
+# n - page length on book, p - page the student wants to turn to
+def pageCount(n, p)
+  # Write your code here; Return the minimum value of pages to turn
+
+end
+
+# T.0: 1 ; turning forward
+puts pageCount(6, 2)
+# T.1: 0 ; turning backward
+puts pageCount(5, 4)
