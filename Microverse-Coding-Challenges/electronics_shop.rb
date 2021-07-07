43814a74c12f02c3e48d99d5c45a7ac3b67a2e53
Alfredo C <alfredo_cardenas@live.com>
2021-07-07 15:05:51 -0500

    Add basic code definition and base test cases to solve

---
+++ Microverse-Coding-Challenges/electronics_shop.rb
@@ -0,0 +1,15 @@
+# Det the most expensive keyboard and USB that can be bought
+# return -1 if can't buy both
+def getMoneySpent(keyboards, drives, b)
+  #
+  # Write your code here.
+  #
+
+end
+
+# T.C.example : 
+puts getMoneySpent([40,50,60], [5,8,12], 60)
+# T.C.0 : 9
+puts getMoneySpent([3,1], [5,2,8], 10)
+# T.C.0 : -1
+puts getMoneySpent([4], [5], 5)
