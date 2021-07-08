129813bf723a6205a04b3a7edcaeb8d12ae6bcad
Alfredo C <alfredo_cardenas@live.com>
2021-07-08 15:35:17 -0500

    Found solution. Passed 16/16

--- Microverse-Coding-Challenges/electronics_shop.rb
+++ Microverse-Coding-Challenges/electronics_shop.rb
@@ -1,10 +1,21 @@
 # Det the most expensive keyboard and USB that can be bought
 # return -1 if can't buy both
 def getMoneySpent(keyboards, drives, b)
-  #
   # Write your code here.
-  #
+  asc_keyboards = keyboards.sort()
+  asc_drives = drives.sort()
+  m_expensive = []
 
+  # return -1 if asc_keyboards[-1] >= b || asc_drives[-1] >= b
+
+  keyboards.each do |kb|
+    drives.each do |dr|
+      sum = kb + dr
+      m_expensive.push(sum) if sum <= b
+    end
+  end
+  return -1 if m_expensive.empty?
+  m_expensive.max()
 end
 
 # T.C.example : 
