7c818bd0452af03acfe99cd3648185199f81ab1a
Alfredo C <alfredo_cardenas@live.com>
2021-07-15 15:44:05 -0500

    Add default challenge function with base TCs

---
+++ Microverse-Coding-Challenges/picking_numbers.rb
@@ -0,0 +1,11 @@
+# find the longest subarray where the absolute difference
+# between any two elements is less than or equal to 1
+def pickingNumbers(a)
+  # Write your code here
+
+end
+
+# T.C.0 : 3
+puts pickingNumbers([4,6,5,3,3,1])
+# T.C.1 : 5
+puts pickingNumbers([1,2,2,3,1,2])
