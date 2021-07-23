2ea1b86e21e89dccac58339bb7a3c036e9ab9585
Alfredo C <alfredo_cardenas@live.com>
2021-07-23 18:57:34 -0500

    Add default function call and base test cases for hurdle race chllg

---
+++ Microverse-Coding-Challenges/hurdle_race.rb
@@ -0,0 +1,9 @@
+def hurdleRace(k, height)
+  # minimum potions needed to jump all hurdles? If none, return 0
+    # Write your code here
+end
+
+# T.C.0 : 2
+puts hurdleRace(4, [1,6,3,5,2])
+# T.C.1 : 0
+puts hurdleRace(7, [2,5,4,5,2])
