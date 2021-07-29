bc4cf82536b56321b5bdad0363c5f2158dff83c7
Alfredo C <alfredo_cardenas@live.com>
2021-07-28 19:04:17 -0500

    Add default function call and base (2) test cases

---
+++ Microverse-Coding-Challenges/angry_professor.rb
@@ -0,0 +1,11 @@
+# Determine IF the class IS CANCELED by the amount of students(a) arrived and on time(k, threshold)
+# On time <= 0. Return 'YES' or 'NO'
+def angryProfessor(k, a)
+    # Write your code here
+
+end
+
+# T.C.ex0 : YES
+puts angryProfessor(3, [-1,-3,4,2])
+# T.C.ex1 : NO
+puts angryProfessor(2, [0,-1,2,1])
