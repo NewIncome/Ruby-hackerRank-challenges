fadb72e8a51bf8d4f1b622fc69627b2396744e06
Alfredo C <alfredo_cardenas@live.com>
2021-07-29 19:11:24 -0500

    Add default function call and base test cases (2)

---
+++ Microverse-Coding-Challenges/find_digits.rb
@@ -0,0 +1,10 @@
+# Return the amount of divisible numbers from it's own digits
+def findDigits(n)
+  # Write your code here
+
+end
+
+# T.C.ex0 : 2
+puts findDigits(12)
+# T.C.ex1 : 3
+puts findDigits(1012)
