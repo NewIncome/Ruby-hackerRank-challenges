e2160991946d88c1d8ac3bc133071d15726714ff
Alfredo C <alfredo_cardenas@live.com>
2021-08-06 09:20:57 -0500

    Add default function call and base test cases for library fine chllg

---
+++ Microverse-Coding-Challenges/library_fine.rb
@@ -0,0 +1,9 @@
+# Define the number of hackos to pay for the lateness of the
+# expected-return-date(d1) compared to the due-date(d2)
+def libraryFine(d1, m1, y1, d2, m2, y2)
+  # Write your code here
+
+end
+
+# T.C.0 : 45
+puts libraryFine(9,6,2015,6,6,2015)
