9e43c894bbb9432dbd56a275025d38960d692e24
Alfredo C <alfredo_cardenas@live.com>
2021-08-16 12:41:38 -0500

    Add default function call and base test cases

---
+++ Microverse-Coding-Challenges/repeated_string.rb
@@ -0,0 +1,11 @@
+# return the amount of letters 'a', up-to-nth position in
+# a repeated string
+def repeatedString(s, n)
+  # Write your code here
+
+end
+
+# T.C.0 : 7
+puts repeatedString('aba')
+# T.C.1 : 1000000000000
+puts repeatedString('a')
