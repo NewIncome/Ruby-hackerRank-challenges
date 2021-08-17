2a85462c50e72d364cba6d80d38cae9cb74d31a7
Alfredo C <alfredo_cardenas@live.com>
2021-08-17 15:43:49 -0500

    Code a first approach to solution. Passed 6/23 TC's.

--- Microverse-Coding-Challenges/repeated_string.rb
+++ Microverse-Coding-Challenges/repeated_string.rb
@@ -2,7 +2,14 @@
 # a repeated string
 def repeatedString(s, n)
   # Write your code here
-
+  # Non-math approach
+  return n if s == 'a'
+  a_reps = 0
+  long_str = n % s.length == 0 ? s * (n/s.length) : s * (n/s.length + 1)
+  for i in 0..n
+    a_reps += 1 if long_str[i] == 'a'
+  end
+  a_reps
 end
 
 # T.C.0 : 7
