737e50c3209b068142b5959d3c64017116d06197
Alfredo C <alfredo_cardenas@live.com>
2021-08-18 10:33:40 -0500

    Add first try for solution. Failing most TC's. Try with while loop

--- Microverse-Coding-Challenges/equialize_array.rb
+++ Microverse-Coding-Challenges/equialize_array.rb
@@ -1,7 +1,20 @@
 
 def equalizeArray(arr)
   # Write your code here
-
+  cnt = 0
+  while arr.length > 1
+    chk = arr.shift
+    puts "arr.include?(#{chk}): #{arr.include?(chk)}"
+    if arr.include?(chk)
+      # does include it, find && remove next element
+      arr.slice!(arr.index(chk))
+    else
+      # doesn't include it, add 1
+      cnt += 1
+    end
+    puts "array: #{arr}, cnt: #{cnt}"
+  end
+  cnt
 end
 
 # T.C.0 : 2
