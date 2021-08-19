60c9167ccbb217714149aebf50083f88809c67d4
Alfredo C <alfredo_cardenas@live.com>
2021-08-19 15:35:13 -0500

    2nd try code. Passed 15/18 TC's. Count arr elements with a hash and then sum the ones that are not the max value.

--- Microverse-Coding-Challenges/equialize_array.rb
+++ Microverse-Coding-Challenges/equialize_array.rb
@@ -1,19 +1,28 @@
 
 def equalizeArray(arr)
   # Write your code here
-  cnt = 0
-  while arr.length > 1
-    chk = arr.shift
-    puts "arr.include?(#{chk}): #{arr.include?(chk)}"
-    if arr.include?(chk)
-      # does include it, find && remove next element
-      arr.slice!(arr.index(chk))
-    else
-      # doesn't include it, add 1
-      cnt += 1
-    end
-    puts "array: #{arr}, cnt: #{cnt}"
-  end
+  # # ---1st try NOPE---
+  # cnt = 0
+  # while arr.length > 1
+  #   chk = arr.shift
+  #   puts "arr.include?(#{chk}): #{arr.include?(chk)}"
+  #   if arr.include?(chk)
+  #     # does include it, find && remove next element
+  #     arr.slice!(arr.index(chk))
+  #   else
+  #     # doesn't include it, add 1
+  #     cnt += 1
+  #   end
+  #   puts "array: #{arr}, cnt: #{cnt}"
+  # end
+  # cnt
+
+  # ---2nd try---
+  ob = Hash.new
+  max = 0
+  arr.each{|e| ob[e] = (ob[e] || 0) + 1; max = ob[e] if ob[e] > max}
+  puts ob
+  ob.each{|k,v| cnt+=v if v != max}
   cnt
 end
 
